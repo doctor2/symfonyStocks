79bc5c493073d8682c5ae2252d7f940184528dd2
Ivan Gulin <doctor254452009@ya.ru>
2021-07-21 15:33:57 +0500

    fix admin controllers view and logic

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,9 +2,7 @@
 
 namespace App\Controller\Admin;
 
-use App\Entity\Stock;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
-use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use EasyCorp\Bundle\EasyAdminBundle\Router\CrudUrlGenerator;
 use Symfony\Component\HttpFoundation\Response;
@@ -21,17 +19,18 @@ class DashboardController extends AbstractDashboardController
         $url = $routeBuilder->setController(StockCrudController::class)->generateUrl();
 
         return $this->redirect($url);
-     }
+    }
 
     public function configureDashboard(): Dashboard
     {
-        return Dashboard::new()
+        return Dashboard::new ()
             ->setTitle('SymfonyStocks');
     }
 
     public function configureMenuItems(): iterable
     {
-        yield MenuItem::linktoDashboard('Dashboard', 'fa fa-home');
-        yield MenuItem::linkToCrud('Stocks', 'fas fa-map-marker-alt', Stock::class);
+        return [];
+        // yield MenuItem::linktoDashboard('Dashboard', 'fa fa-home');
+        // yield MenuItem::linkToCrud('Stocks', 'fas fa-map-marker-alt', Stock::class);
     }
 }
--- src/Controller/Admin/StockCrudController.php
+++ src/Controller/Admin/StockCrudController.php
@@ -3,11 +3,16 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Stock;
+use Doctrine\ORM\QueryBuilder;
+use EasyCorp\Bundle\EasyAdminBundle\Collection\FieldCollection;
+use EasyCorp\Bundle\EasyAdminBundle\Collection\FilterCollection;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Dto\EntityDto;
+use EasyCorp\Bundle\EasyAdminBundle\Dto\SearchDto;
 use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
@@ -16,6 +21,7 @@ use EasyCorp\Bundle\EasyAdminBundle\Field\PercentField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Filter\BooleanFilter;
+use EasyCorp\Bundle\EasyAdminBundle\Filter\NumericFilter;
 
 class StockCrudController extends AbstractCrudController
 {
@@ -35,6 +41,9 @@ class StockCrudController extends AbstractCrudController
     {
         return $filters
             ->add(BooleanFilter::new ('isTracked'))
+            ->add(NumericFilter::new ('sixMonthsMinimumPercent'))
+            ->add(NumericFilter::new ('sixMonthsMaximumPercent'))
+            ->add(NumericFilter::new ('weekOpenPercent'))
         ;
     }
 
@@ -48,6 +57,7 @@ class StockCrudController extends AbstractCrudController
         yield NumberField::new ('sixMonthsMinimum', 'Minimum')->setFormTypeOption('disabled', true);
         yield NumberField::new ('weekOpen')->setFormTypeOption('disabled', true);
         yield NumberField::new ('current')->setFormTypeOption('disabled', true);
+        yield PercentField::new ('sixMonthsMinimumPercent', 'MinimumPercent')->setFormTypeOption('disabled', true);
         yield PercentField::new ('sixMonthsMaximumPercent', 'MaximumPercent')->setFormTypeOption('disabled', true);
         yield PercentField::new ('weekOpenPercent')->setFormTypeOption('disabled', true);
         yield TextareaField::new ('usefulLinks')->renderAsHtml();
@@ -63,5 +73,10 @@ class StockCrudController extends AbstractCrudController
         } else {
             yield $updatedAt;
         }
+    }
+
+    public function createIndexQueryBuilder(SearchDto $searchDto, EntityDto $entityDto, FieldCollection $fields, FilterCollection $filters): QueryBuilder
+    {
+        return parent::createIndexQueryBuilder($searchDto, $entityDto, $fields, $filters)->andWhere('entity.current IS NOT NULL');
     }
 }
